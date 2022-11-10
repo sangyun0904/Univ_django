edf8f9add052313df48d40887d57f70e658a34d0
Sang Yoon <sangyun0904@gmail.com>
2022-11-10 22:09:45 +0900

    Modify polls views.py and index.html

--- meta_contents/polls/views.py
+++ meta_contents/polls/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
 from django.utils import timezone
 from django.shortcuts import render
+from django.template import loader
 
 def index(request):
-    return render(request, 'polls/index.html')
+    now = timezone.now().strftime("%H : %M %p")
+    template = loader.get_template('polls/index.html')
+    context = {
+        'time_now': now
+    }
+    return HttpResponse(template.render(context, request))
 
 
 
