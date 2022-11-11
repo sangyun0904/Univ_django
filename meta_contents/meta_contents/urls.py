69106e2332168cf1bc1b019d722036ffeadd7e82
sangyoon kim <sangyun0904@gmail.com>
2022-11-11 14:05:21 +0900

    Connect login to polls

--- meta_contents/meta_contents/urls.py
+++ meta_contents/meta_contents/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('polls.urls')),
-    path('login/', include('login.urls')),
+    path('polls/', include('polls.urls')),
+    path('', include('login.urls')),
     path('admin/', admin.site.urls),
 ]
