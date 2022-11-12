ad5bec37dd14f94891f1abb27e7e3830c85ce8d6
sangyoon kim <sangyun0904@gmail.com>
2022-11-12 15:50:05 +0900

    Modify polls index.js make background into class

--- meta_contents/polls/static/js/index.js
+++ meta_contents/polls/static/js/index.js
@@ -4,43 +4,61 @@ const c = canvas.getContext('2d')
 canvas.width = 768
 canvas.height = 432
 
-var img_cnt = 0
-
 const image = new Image()
 image.src = '/static/img/map.png'
 
 const playerImage = new Image()
 playerImage.src = '/static/img/down.png'
 
-image.onload = () => {
-    img_cnt++
-    tryDraw()
-}
+class Sprite {
+    constructor({ position, velocity, image }) {
+        this.position = position
+        this.image = image
+    }
 
-playerImage.onload = () => {
-    img_cnt++
-    tryDraw()
-}
-
-function tryDraw()
-{
-    if (img_cnt == 2)
-    {
-        c.drawImage(image, -110, -230)
-        c.drawImage(
-            playerImage,
-            0,
-            0,
-            playerImage.width / 4,
-            playerImage.height,
-            canvas.width / 2 - playerImage.width / 4,
-            canvas.height / 2 - playerImage.height / 4,
-            playerImage.width / 4,
-            playerImage.height
-        )
+    draw() {
+        c.drawImage(this.image, -110, -230)
     }
 }
 
-window.addEventListener('keydown', () => {
-    console.log("keydown")
+const background = new Sprite({
+    position: {
+        x: -110,
+        y: -230
+    },
+    image: image
+})
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    background.draw()
+    c.drawImage(
+        playerImage,
+        0,
+        0,
+        playerImage.width / 4,
+        playerImage.height,
+        canvas.width / 2 - playerImage.width / 4,
+        canvas.height / 2 - playerImage.height / 4,
+        playerImage.width / 4,
+        playerImage.height
+    )
+}
+animate()
+
+window.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'ArrowUp':
+            console.log('up')
+            break
+        case 'ArrowDown':
+            console.log('down')
+            break
+        case 'ArrowLeft':
+            console.log('left')
+            break
+        case 'ArrowRight':
+            console.log('right')
+            break
+    }
 })
