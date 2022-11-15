260f9d794a34df447f308e8e1fe7e6551ecbc810
sangyoon kim <sangyun0904@gmail.com>
2022-11-15 15:51:32 +0900

    Add login room.js modify index templates

--- meta_contents/polls/static/js/index.js
+++ meta_contents/polls/static/js/index.js
@@ -143,34 +143,36 @@ window.addEventListener('keyup', (e) => {
 })
 
 //chat socket
-const chatSocket = new WebSocket("ws://" + window.location.host + "/");
 
-chatSocket.onopen = function (e) {
-console.log("The connection was setup successfully !");
+const chatSocket = new WebSocket(
+    'ws://'
+    + window.location.host
+    + '/ws/polls/'
+    + roomname
+    + '/'
+);
+
+chatSocket.onmessage = function(e) {
+    const data = JSON.parse(e.data);
+    document.querySelector("#id_chat_item_container").value += (data.message + '\n');
 };
 
-chatSocket.onclose = function (e) {
-console.log("Something unexpected happened !");
+chatSocket.onclose = function(e) {
+    console.error('Chat socket closed unexpectedly');
 };
 
 document.querySelector("#id_message_send_input").focus();
+document.querySelector("#id_message_send_input").onkeyup = function(e) {
+    if (e.keyCode == 13) { // enter, return
+        document.querySelector("#id_message_send_button").click();
+    }
+};
 
-document.querySelector("#id_message_send_input").onkeyup = function (e) {
-if (e.keyCode == 13) {
-    document.querySelector("#id_message_send_button").click();
+document.querySelector("#id_message_send_button").onclick = function(e) {
+    const messgaeInputDom = document.querySelector("#id_message_send_input");
+    const message = messgaeInputDom.value;
+    chatSocket.send(JSON.stringify({
+        'message': message
+    }));
+    messgaeInputDom.value = '';
 }
-};
-
-document.querySelector("#id_message_send_button").onclick = function (e) {
-var messageInput = document.querySelector(
-    "#id_message_send_input"
-).value;
-chatSocket.send(JSON.stringify({ message: messageInput, username : "{{request.user.username}}"}));
-};
-chatSocket.onmessage = function (e) {
-const data = JSON.parse(e.data);
-var div = document.createElement("div");
-div.innerHTML = data.username + " : " + data.message;
-document.querySelector("#id_message_send_input").value = "";
-document.querySelector("#id_chat_item_container").appendChild(div);
-};
--- meta_contents/polls/urls.py
+++ meta_contents/polls/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='polls-page'),
+    path('<str:room_name>/', views.index, name='polls-page'),
 ]
