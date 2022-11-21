00e02334f815006c9fedf0d468e9617320f23568
sangyoon kim <sangyun0904@gmail.com>
2022-11-21 14:40:01 +0900

    Install channels-redis -v 3.4.1 modify settings.py port to 6379 and polls consumers.py

--- meta_contents/meta_contents/settings.py
+++ meta_contents/meta_contents/settings.py
@@ -47,7 +47,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND' : 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6378)],
+            "hosts": [('127.0.0.1', 6379)],
         }
     }
 }
