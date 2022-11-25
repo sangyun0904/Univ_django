8a35ef1029e388e2979e8a03fed4353178d1e5cf
sangyoon kim <sangyun0904@gmail.com>
2022-11-25 15:41:07 +0900

    modify signup.js

--- meta_contents/login/static/js/signup.js
+++ meta_contents/login/static/js/signup.js
@@ -2,13 +2,26 @@
 /* ===================================================
     [Validate]
 */
-    var pass = $('#pass');
-    var re_pass = $('#re_pass');
-
+    
     $('.register-form').on('submit', function(){
+        var username = $('#name').val();
+        var pass = $('#pass').val();
+        var re_pass = $('#re_pass').val();
+
         var check = true;
-        alert("Those password doesn't match try again!")
-        return pass == re_pass;
+        console.log(username, pass, re_pass, username.length);
+
+        if (username.length < 1) {
+            alert("must be more than 1 characters");
+            return false;
+        }
+
+        else if (pass != re_pass) {
+            alert("Those password doesn't match try again!");
+            return false;
+        }
+     
+        return false;
     })
 
 })(jQuery);
