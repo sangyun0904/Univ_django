81c235f31adcf09e879f45ae01522f6f789a573d
sangyoon kim <sangyun0904@gmail.com>
2022-11-25 15:46:36 +0900

    modify views.py signup

--- meta_contents/login/views.py
+++ meta_contents/login/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, redirect
 from django.template import loader
 from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import User authenticate, login
 
 from django.contrib import messages
 
 def index(request):
-    print(request.method)
     if request.method == 'POST':
         username = request.POST['email']
         password = request.POST['pass']
@@ -20,6 +19,14 @@ def index(request):
     return render(request, 'login/signin.html', context)
 
 def signup(request):
+    if request.method == 'POST':
+        username = request.POST('name')
+        email = request.POST('email')
+        password = request.POST('pass')
+
+        user = User.objects.create_user(username, email, password)
+        user.save()
+
     template = loader.get_template('login/signup.html')
     context = {}
     return HttpResponse(template.render(context, request))
